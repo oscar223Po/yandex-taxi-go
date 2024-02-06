10581021fed0ebaffceef916d58d81b1313da9a3
Oscar <oscaritoparapito225@mail.ru>
2024-02-06 12:50:56 +0600

    first commit

---
+++ src/js/files/script.js
@@ -0,0 +1,8 @@
+// ~~~~~~~~[ JavaScript Files Connecting ]~~~~~~~
+// Підключення функціоналу "Чертоги Фрілансера"
+import { isMobile } from "./functions.js";
+// Підключення списку активних модулів
+import { flsModules } from "./modules.js";
+// ================[ JavaScript Section ]================
+
+//--------------------------------------------------------------
